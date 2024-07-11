fef54f3bd81946c387c04327a0d99b5e2c1efd92
swati-mp <8swatipatil21@example.com>
2024-07-11 17:13:15 +0530

    commit

--- src/get_data.py
+++ src/get_data.py
@@ -18,7 +18,7 @@ def read_params(config_path):
         config=yaml.safe_load(yaml_file)
         return config
 
-if __name__=="__main___":
+if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
